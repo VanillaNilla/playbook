73f047936afc984bde094e3f368f08296e9422da
Alex <a19310188@gmail.com>
2022-04-25 00:36:25 -0500

    Adding Onboarding and Week 1 (1-3) exercises from LaunchX (Backend)

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,6 @@
+// node main.js
+
+const logger = require('./logger') //Aqui es esta importando la función del archivo logger.js
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
